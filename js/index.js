696692790e59f5fbf2599d3e9e1b4bb09d40e9eb
Andrew Sliwinski <thisandagain@gmail.com>
2013-01-24 23:34:44 -0800

    Rollback on modal/ajax form (for now)

--- js/index.js
+++ js/index.js
@@ -8,7 +8,6 @@
     /**
      * Configuration
      */
-    var formUri = 'https://docs.google.com/spreadsheet/viewform?formkey=dFFZc2hBY1h3M3hPM1B2bVphZmhsY0E6MA#gid=0';
 
     /**
      * Storage objects
@@ -61,7 +60,7 @@
         layer = new google.maps.FusionTablesLayer({
             query: {
                 select: 'Location',
-                from: '1tteiG-HYAlsmh3ef5U-XVDEWu5QXqDxqWwDx-pc',
+                from:   '1tteiG-HYAlsmh3ef5U-XVDEWu5QXqDxqWwDx-pc',
                 // where: '\'Category Tags\' IN(\'#electronics\') AND \'Provider Tags\' IN(\'supplier\')'
             },
             map: map
@@ -72,24 +71,7 @@
      * UI events
      */
     function initEventListeners () {
-        // Cache selectors
-        var $modal = $('#modal');
-
-        // Modal
-        $modal.modal({
-            backdrop:   false,
-            show:       false,
-            remote:     formUri
-        });
-
-        $modal.on('show', function () {
-            $(this).find('head').html('');
-            $(this).find('form').submit(function () {
-                var action = $(this).attr('action');
-                console.log(action);
-                return false;
-            });
-        });
+        
     }
 
     /**
